01ad2955aa2cccc85330c3b514569720778e6da0
Adriano Machado <adrianodonisete@gmail.com>
2022-10-20 17:36:54 -0300

    start project first API

---
+++ Migrations/20221019213014_AddTagInfo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_first.Migrations
+{
+    public partial class AddTagInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Info",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Info",
+                table: "Products");
+        }
+    }
+}
